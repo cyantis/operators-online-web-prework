46837430b02af46c0105a94ac46fc2b69e65bd30
Cody Yantis <cyantis@gmail.com>
2019-01-23 02:06:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,12 @@
 def unsafe?(speed)
-	speed > 60 || speed < 40 ? return true : return false
+	if speed > 60 || speed < 40
+		return true 
+	else
+		return false
+	end
 end
 
 
 def not_safe?(speed)
-
+	speed > 60 || speed < 40 ? return true : return false
 end
